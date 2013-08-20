92907a0979e38b2fb8df75ee9d4305d50fee5607
Zac Stewart <zgstewart@gmail.com>
2013-08-19 22:46:20 -0400

    Use only TextMessages and PasteMessages

--- lib/lita/adapters/campfire/callback.rb
+++ lib/lita/adapters/campfire/callback.rb
@@ -2,29 +2,31 @@ module Lita
   module Adapters
     class Campfire < Adapter
       class Callback
+
+        MESSAGE_TYPES = %w( TextMessage PasteMessage ).freeze
+
         def initialize(robot)
           @robot = robot
         end
 
         def room_message(room)
           room.listen do |m|
-            text    = m.body
-            user    = get_user(m.user)
-            source  = Source.new(user, room)
-            message = Message.new(@robot, text, source)
-            @robot.receive message
+            if MESSAGE_TYPES.include?(m.type)
+              text    = m.body
+              user    = get_user(m.user)
+              source  = Source.new(user, room)
+              message = Message.new(@robot, text, source)
+              @robot.receive message
+            end
           end
         end
 
         private
         def get_user(user_data)
-          if user_data
-            user_id = user_data.id
-            User.new(user_id, user_data)
-          else
-            User.new(-1)
-          end
+          user_id = user_data.delete(:id)
+          User.new(user_id, user_data)
         end
+
       end
     end
   end
