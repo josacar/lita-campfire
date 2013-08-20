ae58bd677738966e5f59f6c06188de2ef7121b7d
Zac Stewart <zgstewart@gmail.com>
2013-08-19 23:13:40 -0400

    Test callbacks

---
+++ spec/lita/adapters/campfire/callback_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe Campfire::Callback do
+
+  class DummyRoom < Struct.new(:message)
+    def listen
+      yield message
+    end
+  end
+
+  let(:campfire_user) { { id: 1, name: 'Bender Bending Rodriguez' } }
+  let(:message) { double('Message') }
+  let(:robot) { double('Robot', mention_name: 'Robot') }
+  let(:room) { DummyRoom.new(campfire_message) }
+  let(:source) { double('Source') }
+  let(:text) { "Yes it's the apocalypse alright. I always though is have a hand in it." }
+  let(:user) { double('User') }
+
+  subject { described_class.new(robot) }
+
+  describe '#room_message' do
+    describe 'with a TextMessage' do
+      let(:campfire_message) { double(
+        type: 'TextMessage',
+        body: text,
+        user: campfire_user) }
+
+      it 'passes the message to Robot#receive' do
+        expect(Lita::User).to receive(:new).with(1, name: 'Bender Bending Rodriguez').and_return(user)
+        expect(Lita::Source).to receive(:new).with(user, room).and_return(source)
+        expect(Lita::Message).to receive(:new).with(robot, text, source).and_return(message)
+        expect(robot).to receive(:receive).with(message)
+        subject.room_message(room)
+      end
+    end
+
+    describe 'with a PasteMessage' do
+      let(:campfire_message) { double(
+        type: 'PasteMessage',
+        body: text,
+        user: campfire_user) }
+
+      it 'passes the message to Robot#receive' do
+        expect(Lita::User).to receive(:new).with(1, name: 'Bender Bending Rodriguez').and_return(user)
+        expect(Lita::Source).to receive(:new).with(user, room).and_return(source)
+        expect(Lita::Message).to receive(:new).with(robot, text, source).and_return(message)
+        expect(robot).to receive(:receive).with(message)
+        subject.room_message(room)
+      end
+    end
+  end
+
+end
