2219376bb6316065fdfb7e98fc115bd06c160b5d
Zac Stewart <zgstewart@gmail.com>
2013-09-01 21:26:23 -0400

    Create user for all the users in a room upon join

--- lib/lita/adapters/campfire/callback.rb
+++ lib/lita/adapters/campfire/callback.rb
@@ -3,28 +3,86 @@ module Lita
     class Campfire < Adapter
       class Callback
 
-        MESSAGE_TYPES = %w( TextMessage PasteMessage ).freeze
+        attr_reader :robot, :room
 
-        def initialize(robot)
+        def initialize(robot, room)
           @robot = robot
+          @room  = room
+
+          @receivers = [MessageReceiver, EnterReceiver].map do |receiver_type|
+            receiver_type.new(self)
+          end
         end
 
-        def room_message(room)
-          room.listen do |m|
-            if MESSAGE_TYPES.include?(m.type)
-              text    = m.body
-              user    = create_user(m.user)
-              source  = Source.new(user, room.id.to_s)
-              message = Message.new(@robot, text, source)
-              @robot.receive message
+        def listen
+          @room.listen do |event|
+            @receivers.each do |receiver|
+              receiver.receive event
             end
           end
         end
 
-        private
+        def register_users
+          @room.users.each do |user|
+            create_user user
+          end
+        end
+
         def create_user(user_data)
+          user_data = user_data.dup
           user_id = user_data.delete(:id)
           User.create(user_id, user_data)
+        end
+
+        class EventReceiver
+
+          class << self
+            attr_accessor :message_types
+
+            def receives(*types)
+              self.message_types = types
+            end
+          end
+
+          def initialize(callback)
+            @callback = callback
+            @robot    = callback.robot
+            @room     = callback.room
+          end
+
+          def receive(event)
+           _receive(event) if receives?(event)
+          end
+
+          private
+
+          def receives?(event)
+            self.class.message_types.include?(event.type)
+          end
+        end
+
+        class MessageReceiver < EventReceiver
+
+          receives 'TextMessage', 'PasteMessage'
+
+          def _receive(event)
+            text    = event.body
+            user    = @callback.create_user(event.user)
+            source  = Source.new(user, @room.id.to_s)
+            message = Message.new(@robot, text, source)
+            @robot.receive message
+          end
+
+        end
+
+        class EnterReceiver < EventReceiver
+
+          receives 'EnterMessage'
+
+          def _receive(event)
+            @callback.create_user(event.user)
+          end
+
         end
 
       end
--- spec/lita/adapters/campfire/callback_spec.rb
+++ spec/lita/adapters/campfire/callback_spec.rb
@@ -2,23 +2,25 @@ require 'spec_helper'
 
 describe Campfire::Callback do
 
-  class DummyRoom < Struct.new(:id, :message)
+  class DummyRoom < Struct.new(:id, :message, :users)
     def listen
       yield message
     end
   end
 
-  let(:campfire_user) { { id: 1, name: 'Bender Bending Rodriguez' } }
-  let(:message)       { double('Message') }
-  let(:robot)         { double('Robot', mention_name: 'Robot') }
-  let(:room)          { DummyRoom.new(1, campfire_message) }
-  let(:source)        { double('Source') }
-  let(:text)          { "Yes it's the apocalypse alright. I always though is have a hand in it." }
-  let(:user)          { double('User') }
+  let(:campfire_message) { double }
+  let(:campfire_user)    { { id: 1, name: 'Bender Bending Rodriguez' } }
+  let(:message)          { double('Message') }
+  let(:robot)            { double('Robot', mention_name: 'Robot') }
+  let(:room)             { DummyRoom.new(1, campfire_message, users) }
+  let(:source)           { double('Source') }
+  let(:text)             { "Yes it's the apocalypse alright. I always though is have a hand in it." }
+  let(:user)             { double('User') }
+  let(:users)            { [ {id: 2, name: 'Bender'}, {id: 3, name: 'Washbucket'} ] }
 
-  subject { described_class.new(robot) }
+  subject { described_class.new(robot, room) }
 
-  describe '#room_message' do
+  describe '#listen' do
     %w( TextMessage PasteMessage ).each do |message_type|
       describe "with a #{message_type}" do
         let(:campfire_message) { double(
@@ -31,9 +33,32 @@ describe Campfire::Callback do
           expect(Lita::Source).to receive(:new).with(user, '1').and_return(source)
           expect(Lita::Message).to receive(:new).with(robot, text, source).and_return(message)
           expect(robot).to receive(:receive).with(message)
-          subject.room_message(room)
+          subject.listen
         end
       end
+    end
+
+    describe 'EnterMessage' do
+      let(:campfire_message) { double(
+        type: 'EnterMessage',
+        user: campfire_user) }
+
+      it 'creates a user' do
+        expect(Lita::User).to receive(:create).with(1, name: 'Bender Bending Rodriguez').and_return(user)
+        subject.listen
+      end
+    end
+
+  end
+
+  describe '#register_users' do
+    it 'creates a user for all the users in the room' do
+      users.each do |user|
+        user_id = user[:id]
+        name    = user[:name]
+        expect(Lita::User).to receive(:create).with(user_id, name: name)
+      end
+      subject.register_users
     end
   end
 
